32d0b02e5b221bcc63f891005bbe3756f1feaf27
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-08-19 13:45:49 +0100

    create a json file for all packs

--- Data.js
+++ Data.js
@@ -14,8 +14,266 @@ const techBro={
         },
         {
             quantity:2,
+            item:"Joggers"
+        },
+        {
+            quantity:2,
+            item:"Jersey"
+        },
+        {
+            quantity:3,
+            item:"Plain tees"
+        },
+        {
+            quantity:1,
+            item:"Crocs"
+        },
+        {
+            quantity:3,
+            item:"Designer tees"
+        }
+
+    ]
+};
+const student = {
+    items: [
+        {
+            quantity:3,
+            item:"T-Shirts"
+        },
+        {
+            quantity:1,
             item:"Jeans"
+        },
+        {
+            quantity:2,
+            item:"Pant trousers"
+        },
+        {
+            quantity:2,
+            item:"Collared Shirts"
+        },
+        {
+            quantity:3,
+            item:"Polo Shirts"
+        },
+        {
+            quantity:2,
+            item:"Chinos"
+        },
+        {
+            quantity:1,
+            item:"Palm sandal"
+        },
+        {
+            quantity:1,
+            item:"Loafers"
+        }
+    ]
+
+};
+const workFromHome = {
+    items: [
+        {
+            quantity:2,
+            item:"Jeans"
+        },
+        {
+            quantity:1,
+            item:"Sweater/Cardigan"
+        },
+        {
+            quantity:2,
+            item:"Kakki pants"
+        },
+        {
+            quantity:2,
+            item:"Joggers"
+        },
+        {
+            quantity:2,
+            item:"Jersey"
+        },
+        {
+            quantity:3,
+            item:"Plain tees"
+        },
+        {
+            quantity:3,
+            item:"Designer tees"
         },
 
     ]
-}
+};
+const rainingSeason  = {
+    items: [
+        {
+            quantity:3,
+            item:"Dark coloured shirts"
+        },
+        {
+            quantity:2,
+            item:"Black jeans"
+        },
+        {
+            quantity:1,
+            item:"Grey Pants"
+        },
+        {
+            quantity:2,
+            item:"Hoodies"
+        },
+        {
+            quantity:1,
+            item:"Leather Jacket"
+        },
+        {
+            quantity:1,
+            item:"Denim Jacket"
+        },
+        {
+            quantity:2,
+            item:"Turtle Neck"
+        }
+    ]
+};
+const partyGoer = {
+    items: [
+        {
+            quantity:3,
+            item:"Patterned Shirt"
+        },
+        {
+            quantity:2,
+            item:"Jeans"
+        },
+        {
+            quantity:1,
+            item:"Denim Jackets"
+        },
+        {
+            quantity:3,
+            item:"Chinos"
+        },
+        {
+            quantity:2,
+            item:"Pants"
+        },
+        {
+            quantity:2,
+            item:"Aloha Shirts"
+        },
+        {
+            quantity:2,
+            item:"Sneakers"
+        }
+    ]
+};
+const owanbe = {
+    items: [
+        {
+            quantity:2,
+            item:"Senator"
+        },
+        {
+            quantity:1,
+            item:"Loafers"
+        },
+        {
+            quantity:1,
+            item:"Agbada"
+        },
+        {
+            quantity:2,
+            item:"Ankara shorts"
+        },
+        {
+            quantity:1,
+            item:"Brogues"
+        },
+        {
+            quantity:1,
+            item:"Palms"
+        }
+    ]
+};
+const gym = {
+    items: [
+        {
+            quantity:5,
+            item:"Racerbacks"
+        },
+        {
+            quantity:4,
+            item:"Sleeveless T-Shirts"
+        },
+        {
+            quantity:4,
+            item:"Sweatshorts"
+        },
+        {
+            quantity:3,
+            item:"Sweatpants"
+        },
+        {
+            quantity:3,
+            item:"Trainers"
+        }
+    ]
+};
+const dateNight = {
+    items: [
+        {
+            quantity:3,
+            item:"Collared shirts"
+        },
+        {
+            quantity:2,
+            item:"Blazers"
+        },
+        {
+            quantity:2,
+            item:"Jeans"
+        },
+        {
+            quantity:2,
+            item:"Pants"
+        },
+        {
+            quantity:1,
+            item:"Accesories (Wrist Watch)"
+        },
+        {
+            quantity:1,
+            item:"Loafers"
+        }
+    ]
+};
+const corporateCasual= {
+    items: [
+        {
+            quantity:2,
+            item:"Blazers Jacket"
+        },
+        {
+            quantity:2,
+            item:"Pant trousers"
+        },
+        {
+            quantity:2,
+            item:"Turtle Neck"
+        },
+        {
+            quantity:3,
+            item:"Plain round neck shirt"
+        },
+        {
+            quantity:2,
+            item:"Loafers"
+        },
+        {
+            quantity:1,
+            item:"White sneakers"
+        }
+    ]
+};
