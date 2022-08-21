1c0a3a9cd929e4c52430515a2f7fcf448de2ae15
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-08-21 11:06:44 +0100

    create unique id to all pack items

--- Data.js
+++ Data.js
@@ -1,34 +1,42 @@
 const techBro={
     items:[
         {
+            id: 1,
             quantity:2,
             item:"Jeans"
         },
         {
+            id: 2,
             quantity:1,
             item:"Sweater/Cardigan"
         },
         {
+            id: 3,
             quantity:2,
             item:"Kakki pants"
         },
         {
+            id: 4,
             quantity:2,
             item:"Joggers"
         },
         {
+            id: 5,
             quantity:2,
             item:"Jersey"
         },
         {
+            id: 6,
             quantity:3,
             item:"Plain tees"
         },
         {
+            id: 7,
             quantity:1,
             item:"Crocs"
         },
         {
+            id: 8,
             quantity:3,
             item:"Designer tees"
         }
@@ -38,34 +46,42 @@ const techBro={
 const student = {
     items: [
         {
+            id: 9,
             quantity:3,
             item:"T-Shirts"
         },
         {
+            id: 10,
             quantity:1,
             item:"Jeans"
         },
         {
+            id: 11,
             quantity:2,
             item:"Pant trousers"
         },
         {
+            id: 12,
             quantity:2,
             item:"Collared Shirts"
         },
         {
+            id: 13,
             quantity:3,
             item:"Polo Shirts"
         },
         {
+            id: 14,
             quantity:2,
             item:"Chinos"
         },
         {
+            id: 15,
             quantity:1,
             item:"Palm sandal"
         },
         {
+            id: 16,
             quantity:1,
             item:"Loafers"
         }
@@ -75,30 +91,37 @@ const student = {
 const workFromHome = {
     items: [
         {
+            id: 17,
             quantity:2,
             item:"Jeans"
         },
         {
+            id: 18,
             quantity:1,
             item:"Sweater/Cardigan"
         },
         {
+            id: 19,
             quantity:2,
             item:"Kakki pants"
         },
         {
+            id: 19,
             quantity:2,
             item:"Joggers"
         },
         {
+            id: 20,
             quantity:2,
             item:"Jersey"
         },
         {
+            id: 21,
             quantity:3,
             item:"Plain tees"
         },
         {
+            id: 22,
             quantity:3,
             item:"Designer tees"
         },
@@ -108,30 +131,37 @@ const workFromHome = {
 const rainingSeason  = {
     items: [
         {
+            id: 23,
             quantity:3,
             item:"Dark coloured shirts"
         },
         {
+            id: 24,
             quantity:2,
             item:"Black jeans"
         },
         {
+            id: 25,
             quantity:1,
             item:"Grey Pants"
         },
         {
+            id: 26,
             quantity:2,
             item:"Hoodies"
         },
         {
+            id: 27,
             quantity:1,
             item:"Leather Jacket"
         },
         {
+            id: 28,
             quantity:1,
             item:"Denim Jacket"
         },
         {
+            id: 29,
             quantity:2,
             item:"Turtle Neck"
         }
@@ -140,30 +170,37 @@ const rainingSeason  = {
 const partyGoer = {
     items: [
         {
+            id: 30,
             quantity:3,
             item:"Patterned Shirt"
         },
         {
+            id: 31,
             quantity:2,
             item:"Jeans"
         },
         {
+            id: 32,
             quantity:1,
             item:"Denim Jackets"
         },
         {
+            id: 33,
             quantity:3,
             item:"Chinos"
         },
         {
+            id: 34,
             quantity:2,
             item:"Pants"
         },
         {
+            id: 35,
             quantity:2,
             item:"Aloha Shirts"
         },
         {
+            id: 36,
             quantity:2,
             item:"Sneakers"
         }
@@ -172,26 +209,32 @@ const partyGoer = {
 const owanbe = {
     items: [
         {
+            id: 37,
             quantity:2,
             item:"Senator"
         },
         {
+            id: 38,
             quantity:1,
             item:"Loafers"
         },
         {
+            id: 39,
             quantity:1,
             item:"Agbada"
         },
         {
+            id: 40,
             quantity:2,
             item:"Ankara shorts"
         },
         {
+            id: 41,
             quantity:1,
             item:"Brogues"
         },
         {
+            id: 42,
             quantity:1,
             item:"Palms"
         }
@@ -200,22 +243,27 @@ const owanbe = {
 const gym = {
     items: [
         {
+            id: 43,
             quantity:5,
             item:"Racerbacks"
         },
         {
+            id: 44,
             quantity:4,
             item:"Sleeveless T-Shirts"
         },
         {
+            id: 45,
             quantity:4,
             item:"Sweatshorts"
         },
         {
+            id: 46,
             quantity:3,
             item:"Sweatpants"
         },
         {
+            id: 47,
             quantity:3,
             item:"Trainers"
         }
@@ -224,26 +272,32 @@ const gym = {
 const dateNight = {
     items: [
         {
+            id: 48,
             quantity:3,
             item:"Collared shirts"
         },
         {
+            id: 49,
             quantity:2,
             item:"Blazers"
         },
         {
+            id: 50,
             quantity:2,
             item:"Jeans"
         },
         {
+            id: 51,
             quantity:2,
             item:"Pants"
         },
         {
+            id: 52,
             quantity:1,
             item:"Accesories (Wrist Watch)"
         },
         {
+            id: 53,
             quantity:1,
             item:"Loafers"
         }
@@ -252,26 +306,32 @@ const dateNight = {
 const corporateCasual= {
     items: [
         {
+            id: 54,
             quantity:2,
             item:"Blazers Jacket"
         },
         {
+            id: 55,
             quantity:2,
             item:"Pant trousers"
         },
         {
+            id: 56,
             quantity:2,
             item:"Turtle Neck"
         },
         {
+            id: 57,
             quantity:3,
             item:"Plain round neck shirt"
         },
         {
+            id: 58,
             quantity:2,
             item:"Loafers"
         },
         {
+            id: 59,
             quantity:1,
             item:"White sneakers"
         }
